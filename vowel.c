eedbee1a9afd2c413092df192add0f60504c8373
pavithraraviselvi <31505530+pavithraraviselvi@users.noreply.github.com>
2018-01-19 20:51:39 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("Enter any integer");
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch==i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='u')
+{
+printf("entered character is vowel");
+}
+else
+{
+printf("Entered character is consonant");
+}
+}
