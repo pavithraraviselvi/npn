4667a250d59d331ed02eb11b5adf0618169823a8
pavithraraviselvi <31505530+pavithraraviselvi@users.noreply.github.com>
2018-01-19 20:59:31 +0530

    Create alp.c

---
+++ alp.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter any alphabet");
+scanf("%c",&ch);
+if(9ch>=65&&ch<=90)||(ch>=97&&ch<=122))
+{
+printf(Entered character is alphabet");
+}
+else
+{
+printf("Entered character is consonant");
+}
+}
