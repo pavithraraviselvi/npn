dfa917a0a5d05ea1344d934271cc82fe40ea2c1c
pavithraraviselvi <31505530+pavithraraviselvi@users.noreply.github.com>
2018-01-20 20:44:21 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,n,k,b=0;
+scanf("%d%d",&n,&k);
+inta[5];
+for(i=1;i<=n;i++);
+{
+scanf("%d",&a[i]);
+}
+for(i=1;i<=k;i++)
+{
+b=b+a[1];
+}
+printf("%d",b);
+return0;
+}
