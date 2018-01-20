600c91b9a93b4df8cb701e6fd60a168dac17005b
pavithraraviselvi <31505530+pavithraraviselvi@users.noreply.github.com>
2018-01-20 21:15:07 +0530

    Create oddint.c

---
+++ oddint.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,f,l;
+printf("Enter the 1st interval");
+scanf("%d",&n);
+printf("enter the 2nd interval");
+scanf("%d",&l);
+for(i=f;i<=l;i++)
+{
+if(i%2==1)
+{
+printf("%d is a odd number");
+}
+else
+{
+printf("%d is a even number");
+}
+return0;
+}
