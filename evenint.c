cf3b959cf6001bd4a359c5f09f2a181229c0e97e
pavithraraviselvi <31505530+pavithraraviselvi@users.noreply.github.com>
2018-01-20 21:04:10 +0530

    Create evenint.c

---
+++ evenint.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,f,l;
+printf("Enter the 1st interval");
+scanf("%d",&l);
+for(i=f;;i<=l;i++)
+{
+if(i%2==0)
+{
+prntf("%d is a even number");
+}
+else
+{
+printf("%d is a odd number");
+}
+return0;
+}
