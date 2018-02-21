9b25c7056a936387c8163d829721197f52ea79b2
pavithraraviselvi <31505530+pavithraraviselvi@users.noreply.github.com>
2018-02-21 16:59:04 +0530

    Create bitswap.c

---
+++ bitswap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a=3,b=2;
+printf("values of a&b before swapping:%d%d",a,b);
+{
+a=a^b;
+b=a^b;
+a=a^b;
+}
+printf("values of a&b after swapping:%d%d",a,b);
+return 0;
+}
