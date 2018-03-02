cc3b80f7a8b70679baff97397d4bfe35c7cf416a
pavithraraviselvi <31505530+pavithraraviselvi@users.noreply.github.com>
2018-03-02 16:49:38 +0530

    Create lowhigh5.c

---
+++ lowhigh5.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int n,a[5],i,j,l,h,temp;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+		if(a[i]>a[j])
+		{
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+		}
+		}
+	}
+	l=a[0];
+	h=a[n-1];
+	printf("%d",l);
+	printf("%d",h);
+	return 0;
+}
