b2ea85f1a0827cbe4638837fa74b6c0509528573
pavithraraviselvi <31505530+pavithraraviselvi@users.noreply.github.com>
2018-03-05 21:34:30 +0530

    Create numletter.c

---
+++ numletter.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+switch(n)
+{
+case1:
+printf("\none");
+break;
+case2:
+printf("\ntwo");
+break;
+case3:
+printf("\nthree");
+break;
+default:
+printf("error");
+}
+return 0;
+}
