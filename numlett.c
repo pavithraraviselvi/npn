107a51879d63f02de02acd00acf4e09372f587cc
pavithraraviselvi <31505530+pavithraraviselvi@users.noreply.github.com>
2018-03-12 10:10:32 +0000

    Create numlett.c

---
+++ numlett.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	switch(n)
+	{
+		case 1:
+		printf("one");
+		break;
+		case 2:
+		printf("Two");
+		break;
+		case 3:
+		printf("Three");
+		break;
+		default:
+		printf("Error");
+		break;
+		
+	}
+	return 0;
+}
